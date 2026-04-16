c5f9941133088ee806937af81be9f8218228ef12
saurav11sarkar <sarkarsaurav11@outlook.com>
2026-04-16 10:46:24 +0600

    feat: add child-insights controller to handle insight retrieval by ID

--- src/app/module/child_insights/child_insights.controller.ts
+++ src/app/module/child_insights/child_insights.controller.ts
@@ -1,18 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  HttpCode,
-  HttpStatus,
-} from '@nestjs/common';
+import { Controller, Get, Param, HttpCode, HttpStatus } from '@nestjs/common';
 import { ChildInsightsService } from './child_insights.service';
-import { CreateChildInsightDto } from './dto/create-child_insight.dto';
-import { UpdateChildInsightDto } from './dto/update-child_insight.dto';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('child-insights')
 @Controller('child-insights')
